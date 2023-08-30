9feb8f1c8894bb6f5cbaffe13a8b7e3c1125814a
lucaszebre <yakirisk@gmail.com>
2023-08-30 14:38:51 +0200

    struggling using psql on localhost -_-

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Pool = require('pg').Pool
+const  dotenv = require('dotenv');
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+dotenv.config();
+const { POSTGRESQL_PASSWORD } = process.env;
+
+const connectionConfig = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'blogpost',
+    password: 'undefined',
+    port: 5432, // The default PostgreSQL port
+};
+
+const client = new Pool(connectionConfig);
+
+async function connectAndQuery() {
+    try {
+        await client.connect();
+        console.log('Connected to PostgreSQL');
+    } catch (error) {
+        console.error('Error:', error.message);
+    } finally {
+        await client.end();
+        console.log('Connection to PostgreSQL closed');
+    }
+}
+
+connectAndQuery();
+
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
