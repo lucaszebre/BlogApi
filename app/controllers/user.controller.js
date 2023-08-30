c987023a649d315f37cc3e37b24a119a7d2beafd
lucaszebre <yakirisk@gmail.com>
2023-08-30 15:59:08 +0200

    add new function in the controller

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -22,5 +22,44 @@ exports.createUser = (req, res) => {
         res.status(201).send(`User added with ID: ${results.rows[0].id}`)
         })
     }
+    exports.updateUser = (req, res) => {
+        const id = parseInt(req.params.userId);
+        const { username, email } = req.body;
+        
+        // Check which fields to update
+        let updateFields = [];
+        if (username) {
+            updateFields.push('username = $1');
+        }
+        if (email) {
+            updateFields.push('email = $2');
+        }
+    
+        // Construct the query dynamically
+        const updateQuery = `
+            UPDATE users
+            SET ${updateFields.join(', ')}
+            WHERE id = $${updateFields.length + 1}
+            RETURNING *
+        `;
+    
+        // Construct parameter array for the query
+        const queryParams = [];
+        if (username) {
+            queryParams.push(username);
+        }
+        if (email) {
+            queryParams.push(email);
+        }
+        queryParams.push(id);
+    
+        pool.query(updateQuery, queryParams, (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(200).json(results.rows);
+        });
+    };
+    
 
     
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,7 +2,7 @@ const controller = require("../controllers/user.controller");
 module.exports = function(app) {
 
     app.get('/user/:userId',controller.getUserById);
-    // app.put('/user/:userId/boards/:boardId',controller.updateBoards);
+    app.put('/user/:userId',controller.updateUser);
     app.post('/user',controller.createUser);
     // app.delete('/user/:userId/boards/:boardId',controller.deleteBoard);
 
