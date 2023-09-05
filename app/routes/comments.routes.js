0eee9fce1e0ac9439459b759bbb339b3d188fcd6
lucaszebre <yakirisk@gmail.com>
2023-09-05 03:39:50 +0200

    make some change into the comments.routes.js

--- app/routes/comments.routes.js
+++ app/routes/comments.routes.js
@@ -3,6 +3,6 @@ const controller = require("../controllers/comments.controller");
 module.exports = function(app) {
     app.get('/user/:userId/post/:postId',isAuthenticated,controller.getComment);
     app.put('/comment/:commentId',isAuthenticated,controller.updateComment);
-    app.post('/comment/:commentId',isAuthenticated,controller.deleteComment);
-    app.delete('/user/:userId/post/:postId/comment/',isAuthenticated,controller.createComment);
+    app.delete('/comment/:commentId',isAuthenticated,controller.deleteComment);
+    app.post('/user/:userId/post/:postId/comment/',isAuthenticated,controller.createComment);
 }
