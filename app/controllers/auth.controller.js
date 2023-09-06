f96c9a14dc2daa2bf9e987517a9fab0872b593a6
lucaszebre <yakirisk@gmail.com>
2023-09-06 07:50:51 +0200

    problem settting the cookie on the render deploy

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -61,7 +61,7 @@ exports.login = async (req, res) => {
                         throw error
                     }
                     })
-            res.cookie('LUCAS-AUTH', sessionToken, { domain: 'localhost', path: '/' });
+            res.cookie('LUCAS-AUTH', sessionToken, { domain: 'onrender.com', path: '/' });
 
                     res.status(200).send({
                         id: results.rows[0].id,
